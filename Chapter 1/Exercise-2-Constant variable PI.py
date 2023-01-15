5fcd7144188ab0439ace615aa7227ed1eb9bc412
Atruba <71628574+Atruba@users.noreply.github.com>
2023-01-15 05:53:29 +0400

    Add files via upload

---
+++ Chapter 1/Exercise-2-Constant variable PI.py
@@ -0,0 +1,8 @@
+radius = float(input(' Please Enter the radius of a circle: '))
+#storing value for PI 
+PI = 3.14159
+Area = PI * radius * radius
+Circumference = PI * radius * 2
+
+print("Area Of a Circle = ",Area)
+print("Circumference Of a Circle = ",Circumference)
