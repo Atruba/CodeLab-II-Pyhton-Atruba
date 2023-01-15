d91b42fe2c1853a9e14b95c776a419807843987a
Atruba <71628574+Atruba@users.noreply.github.com>
2023-01-15 05:54:13 +0400

    Add files via upload

---
+++ Chapter 1/Exercise-3a-User Input Output.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+age = input("Enter age: ")
+hometown = input("Enter your hometown:")
+
+
+print("You're Name is: {}\nYou're Age is: {}\nYou're Hometown is: {}".format(name, age, hometown))
