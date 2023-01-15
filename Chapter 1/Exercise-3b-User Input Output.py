fda746d6972e53fa558c64d29b8d37325fa9f630
Atruba <71628574+Atruba@users.noreply.github.com>
2023-01-15 05:55:07 +0400

    Add files via upload

---
+++ Chapter 1/Exercise-3b-User Input Output.py
@@ -0,0 +1,13 @@
+print("Hello, user!\nWhat is your name?")
+name = input("")
+print("What is your age?")
+age = int(input(""))
+
+titled=name.title()
+
+print("It is good to meet you,",titled)
+print("The length of your name is:\n",len(name))
+
+Age_next = age+1
+
+print("You wil be ",Age_next, "in a year.")
