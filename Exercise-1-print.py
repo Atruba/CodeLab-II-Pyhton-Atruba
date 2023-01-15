20498cc6746ef8e9e20e386fac0c9719be5716d1
Atruba <71628574+Atruba@users.noreply.github.com>
2023-01-15 05:47:16 +0400

    Delete Exercise-1-print.py

--- Exercise-1-print.py
+++
@@ -1,7 +0,0 @@
-
-#The print() function prints the output of the code to the screen.
-
-#The message will be converted into a string.
-
-print('Python uses "interpreter" to convert source code into machine code.')
-print('Interpreter "translates" program one statement at a time.')
