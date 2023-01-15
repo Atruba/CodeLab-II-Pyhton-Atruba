001597fce5b24a5c9abaacc48468bf51b9ef17a9
Atruba <71628574+Atruba@users.noreply.github.com>
2023-01-15 05:55:43 +0400

    Add files via upload

---
+++ Chapter 1/Exercise-4-Number Input Output.py
@@ -0,0 +1,14 @@
+print("Enter 3 numbers to get the values in forward and reversed order")
+
+num_1=int(input("Number 1: "))
+num_2=int(input("Number 2: "))
+num_3=int(input("Number 3: "))
+
+print(f"Your numbers forward: ")
+print(num_1)
+print(num_2)
+print(num_3)
+print(f"Your numbers reversed: ")
+print(num_3)
+print(num_2)
+print(num_1)
